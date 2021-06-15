e944af3ac12bc680c056e486d2388e208729ae7c
EPI <eugen.paslaru@gmail.com>
2021-06-15 05:40:11 +0300

    update project with lib

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+import { MDBSpinningPreloader, MDBBootstrapModulesPro, ToastModule } from 'ng-uikit-pro-standard';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastModule.forRoot(),
+    MDBBootstrapModulesPro.forRoot(),
+  ],
+  providers: [MDBSpinningPreloader],
+  bootstrap: [AppComponent],
+  schemas:      [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
